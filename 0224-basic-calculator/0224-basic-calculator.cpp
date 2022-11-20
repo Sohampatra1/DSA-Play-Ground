0c5b769220a9a446f2b87676189ca45906fe9e9e
Soham patra <74552333+Sohampatra1@users.noreply.github.com>
2022-11-20 20:25:09 +0530

    Time: 96 ms (17.19%), Space: 283.4 MB (5.01%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int i = 0;
+    int calculate(string s) {
+        int ans = 0;
+        int sign = 1;
+        for(i; i<s.size(); i++){
+            if(s[i] == '-'){
+                sign = -1;
+            }
+            else if(s[i] == ')'){
+                return ans;
+            }
+            else if(s[i] == '('){
+                i++;
+                ans += (sign) * calculate(s);
+                sign = 1;
+            }
+            else if(isdigit(s[i])){
+                int num = 0;
+                while(isdigit(s[i])){
+                    num = num*10 + (s[i] - '0');
+                    i++;
+                }
+                ans += (sign)*(num);
+                sign = 1;
+                i--;
+            } else{
+                continue;
+            }
+        }
+        return ans;
+    }
+};
